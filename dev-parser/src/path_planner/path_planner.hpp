7a86d1f5e123edf94f84f22ccc9a1a08042c55e3
CaseyNelson314 <caseynelson314@gmail.com>
2025-08-28 00:31:26 +0900

    update

--- dev-parser/src/path_planner/path_planner.hpp
+++ dev-parser/src/path_planner/path_planner.hpp
@@ -1,3 +1,19 @@
 #pragma once
 
+#include "common/type.hpp"
 
+struct SinglePath
+{
+    float distance;
+
+    float speed_limit;
+
+    float time_acc;
+    float time_const;
+    float time_dec;
+};
+
+SinglePath make_time_block(float begin, float end, float speed_begin, float speed_end, float speed_limit, float acc_limit)
+{
+    
+}
